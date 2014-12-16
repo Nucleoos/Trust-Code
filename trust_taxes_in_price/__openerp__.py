db17913204af972567e9f7279d428ad5d08c8b0a
Mackilem <mack.vdl@gmail.com>
2014-12-16 13:52:32 -0200

    subindo últimas alterações do módulo

---
+++ trust_taxes_in_price/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'trust_taxes_in_price',
+    'description': 'Inclui taxas selecionadas no preço do produto. Ex. (Icms)',
+    'category': 'sale',
+    'license': 'AGPL-3',
+    'author': 'Trust-Code',
+    'website': 'www.trust-code.com.br',
+    'version': '7.0',
+    'depends': ['product', 'account','sale'],
+    'data': ['account_tax_view.xml',],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'active': False,
+}
